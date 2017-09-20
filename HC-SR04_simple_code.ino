b5088de5efd0304ac2a0cd4ff355fbd8738f2677
robodomit <32096476+robodomit@users.noreply.github.com>
2017-09-20 20:58:13 +0300

    Add files via upload

---
+++ HC-SR04_simple_code.ino
@@ -0,0 +1,38 @@
+/*
+ * created by Doru Mandache, http://robodomit.blogspot.ro
+ * 
+ * Hardware Connections:
+        HC-SR04 | Arduino   
+      ----------------------------------------
+         VCC    |  +5V
+         GND    |  GND (any GND pin)
+         Trig   |  Pin 5 or any digital pins
+         Echo   |  Pin 4 or any digital pins
+ */
+
+#define TRIG_PIN  5   // Trig - green Wire  
+#define ECHO_PIN  4   // Echo - blue Wire      
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);    
+} 
+
+void loop() { 
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(4);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  float microseconds = pulseIn(ECHO_PIN, HIGH);
+  float cm = (microseconds / 2) / 29.12;
+  float in = (microseconds / 2) / 74;  
+  Serial.print("Distance in centimeters : ");  
+  Serial.println(cm);  
+  Serial.print("Distance inches : ");  
+  Serial.println(in);
+  delay(500);
+}
+
